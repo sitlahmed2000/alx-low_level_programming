cc00d00b4efd8a12cbff1cb824b905cb28bbea72
Ahmed <sitlahmed2000@gmail.com>
2023-06-16 16:38:21 +0300

    Add 8-print_base.16.c file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print alphabet letters
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c,n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
