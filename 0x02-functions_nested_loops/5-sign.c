22f48755f2026aa0f50329fd978349d1f2d56d5a
Ahmed <sitlahmed2000@gmail.com>
2023-06-20 21:11:15 +0300

    Add sign.c file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_sign - checks for alphabetic character
+ * @n: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}
