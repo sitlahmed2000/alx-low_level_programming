05fcc5a77e50d80d25bf963a8f73f09dcf428c59
Ahmed <sitlahmed2000@gmail.com>
2023-06-20 15:03:04 +0300

    Add 2-print_alphabet_x10 file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet 10 times - print alphabet in lowercase
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	print_alphabet();
+	_putchar('\n');
+}
