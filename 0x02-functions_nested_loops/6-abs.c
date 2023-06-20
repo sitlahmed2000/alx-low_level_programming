a5027f97ed3473a4abbb3275b8da9e61624e4fe8
Ahmed <sitlahmed2000@gmail.com>
2023-06-20 21:38:51 +0300

    Add 6-abs.c file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - checks for alphabetic character
+ * @n: the character to be checked
+ * Return: the corrent answer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
