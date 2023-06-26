b1746f7c893bcc2d48b0c34419ec94765d49d666
Ahmed <sitlahmed2000@gmail.com>
2023-06-26 23:20:59 +0300

    Fix betty style for 3-puts.c file

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- *
+ * _puts - print string of letters
+ * @str: the string will should print it
  */
 
 void _puts(char *str)
 {
 	int n = 0;
 
-	while(str[n] != '\0')
+	while (str[n] != '\0')
 	{
 		_putchar(str[n]);
 		n++;
