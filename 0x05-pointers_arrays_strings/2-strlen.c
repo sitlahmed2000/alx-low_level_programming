86f5b9cfe1edae522d414354f1ab5f5aed817313
Ahmed <sitlahmed2000@gmail.com>
2023-06-26 22:59:24 +0300

    Add 2-strlen.c file and add new line to main.h file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - counting length of the array
+ * @s: The array coming from outside
+ * Return: return n
+ */
+
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+	return (n);
+}
