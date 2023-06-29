62c2df3d1e4a69ba934184b1af28857e99bb3405
Ahmed <sitlahmed2000@gmail.com>
2023-06-29 13:05:55 +0300

    Fix 0-strcat.c file 3

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,24 +8,21 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char *result;
 	int n = 0;
 	int i = 0;
 
 	while (dest[n] != '\0')
 	{
-		result += dest[n];
 		n++;
 	}
 
 	while (src[i] != '\0')
 	{
-		result += src[i];
+		dest[n] = src[i];
 		n++;
 		i++;
 	}
 
-	dest = result;
 	dest[n] = '\0';
 	return (dest);
 }
