01fb6a182a1a1b803441b5161105945561bdff41
Ahmed <sitlahmed2000@gmail.com>
2023-06-29 23:39:13 +0300

    Add 2-strncpy.c file

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+/**
+ * _strncpy - fucntion copy string from place to others
+ * @dest: the place that will past it
+ * @src: the string will copy it
+ * @n: how many letters will copy them
+ * Return: return the dest
+ */
+
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	while (n > 0 && *src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	while (n > 0)
+	{
+		*dest = '\0';
+		dest++;
+		n--;
+	}
+
+	return (dest);
+}
