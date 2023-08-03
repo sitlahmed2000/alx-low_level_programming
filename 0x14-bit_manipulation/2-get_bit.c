767ee44f8ff3ac3901fa2ab165424f6b2577879c
Ahmed <bplahmed28@gmail.com>
2023-08-03 21:03:00 +0300

    Fix 2-get_bit.c file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,12 +8,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int n;
+	unsigned long int i;
 
 	if (index > 64)
 		return (-1);
 
-	n = n >> index;
+	i = n >> index;
 
-	return (n & 1);
+	return (i & 1);
 }
